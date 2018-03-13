1d230a30eb7627bc0e88925fde6a626fc19715ab
TheDarkerPath <37242207+TheDarkerPath@users.noreply.github.com>
2018-03-13 17:46:39 +0000

    Major rework of code
    Multiple players introduced for better reflection of real world play. More checks for div by zero. Much clearer layout of run stats. Improved title for plots.

--- README.md
+++ README.md
@@ -5,26 +5,46 @@ homebrew rules in DnD
 
 Author: Geoff Moore
 
-![fatal_outcomes_20runs_2000turns_98cutoffs](https://user-images.githubusercontent.com/37242207/37293305-cf374828-260a-11e8-979a-ce743902fb1d.png)
+![fatal_outcomes_20_runs_of_2000_rounds_98_and_99_cutoffs](https://user-images.githubusercontent.com/37242207/37359464-8eca82c4-26e5-11e8-9c91-e2bfe7be33bb.png)
 
 Monte Carlo Simulation of fatal outcomes on DnD dice rolls. My current game
 involves homebrew rules with some fatal or character retiring outcomes
-for fumbles and crits. This program simulates this scenario and plots the
-cumulative occurrence of fatal outcomes over time. It also calculates
-probabilities of fatal outcomes from the generated data which can be used
-to check the model against ideal mathematical probabilities.
+for fumbles and crits (we use the blanket term "fatal outcomes" in the simulation
+but we also mean very serious or character-retiring outcomes). This program
+simulates this scenario using a Monte Carlo approach and plots the cumulative
+occurrence of fatal outcomes over time. It also calculates probabilities of
+fatal outcomes from the generated data for each run which can be used to
+check the model against predicted ideal probabilities.
 
-Values for cutoffs for crits and fumbles on the d20 and fatal outcomes
-on the d100 can be varied. The number of turns per run and the number of
-runs can also be adjusted. Runs are plotted as different colour lines and
-superimposed on the same axes for easy comparison. Verticals in the plot
-represent fatal outcomes. The text console output shows summary details
-for each run by default and can be tweaked to display each and every dice
-roll if desired. This is turned off by default to minimise execution time.
+Values can be varied as follows:
+- cutoffs for crits and fumbles on the d20 (must be between 1 and 20)
+- fatal outcome cutoffs on the d100 (can be > 100 which means no fatal
+    outcomes for that type of roll)
+- the number of players
+- the number of turns per run
+- the number of runs
+- whether to display every roll or only show a summary for each run
 
-The counterintuitive aspect of the results from this simulation are that
-unlikely events (ie. fatal outcomes) start to creep into actual dice rolls 
-earlier and more regularly between runs than our intuition would suggest based
-solely on the predicted probabilities. This means that care needs to be taken
-in tuning the fatal outcome cutoffs to arrive at a satisfactory balance of
+For the purposes of the simulation we simplified each round to
+consist of:
+- each player rolling one d20 to attack a monster. We
+  track when a player fumbles their attack (ie. <= d20_fumble_cutoff)
+  AND rolls a fatal outcome on the d100 (ie. >= d100_fatal_fumble_cutoff)
+- the DM rolling one d20 to attack a player (we track when the DM gets a
+  crit (ie. >= d20_crit_cutoff) AND the player rolls a fatal
+  outcome on the d100 (ie. >= d100_fatal_crit_cutoff)
+
+Runs are plotted as different colour lines and superimposed on the same
+axes for easy comparison. Verticals in the plot represent fatal outcomes
+for player characters. The text console output shows summary stats for each run
+by default and but full dice rolls can also be displayed. (CAUTION: this is
+turned off by default to minimise execution time. If changing this setting
+be careful not to run a large simulation or you may find the program
+becomes unresponsive)
+
+The interesting and counterintuitive aspect of the results from this simulation
+is that unlikely events (ie. fatal outcomes) start to creep into simulated dice
+rolls earlier and more regularly runs than our intuition might suggest
+based solely on the predicted probabilities. This means that care needs to be
+taken in tuning the fatal outcome cutoffs to arrive at a satisfactory balance of
 risk versus maintaining enjoyment for players.
--- dnd-fatal-outcome-odds.py
+++ dnd-fatal-outcome-odds.py
@@ -26,20 +26,33 @@ signal.signal(signal.SIGINT, signal_handler)
 #                                                                                                          #
 ############################################################################################################
 # define cutoffs for crits and fumbles on the d20
+# Cutoff values must be within these ranges:
+# 1 < d20_fumble_cutoff < 20
+# 1 < d20_crit_cutoff < 20
 d20_fumble_cutoff = 1
 d20_crit_cutoff = 20
 
-#define cutoffs for fatal or character-retiring outcomes on d100 following crit or fumble on d20
-d100_fatal_fumble_cutoff = 98
+# define cutoffs for fatal or character-retiring outcomes on d100 following crit or fumble on d20
+# for the purposes of this simulation, higher values are worse
+# A crit or fumble cutoff value can be > 100 which means there are no fatal consequences for that type of roll
+d100_fatal_fumble_cutoff = 99
 d100_fatal_crit_cutoff = 98
 
-# define number of turns over which to run the simulation
-number_of_turns = 2000
-
-#define number of runs
+# define number of runs to execute (each run forms a distinct line in the plot)
 number_of_runs = 20
+
+# define number of rounds in each run of the simulation
+number_of_rounds = 2000
+
+# define number of players
+number_of_players = 4
+
+# define whether to output each roll and result (WARNING: for large runs extremely processor intensive)
+# 1 = output each roll
+# 0 = no roll output
+output_each_roll = 0
 ############################################################################################################
-# End of main values for editing                                                                          #
+# End of main values for editing                                                                           #
 #                                                                                                          #
 ############################################################################################################
 
@@ -52,10 +65,20 @@ def rollD100():
     roll = random.randint(1,100)
     return roll
 
+######################################################################################
+# Calculate ideal mathematical odds for d20 crits and fumbles per round, also fatal
+# outcome per round for later comparison with observed stats from simulated data
+predicted_chance_d20_fumble = (d20_fumble_cutoff / 20) * number_of_players
+predicted_chance_d20_crit = (21 - d20_crit_cutoff ) / 20
+predicted_chance_fatal_fumble = predicted_chance_d20_fumble * ( (101 - d100_fatal_fumble_cutoff ) / 100 )
+predicted_chance_fatal_crit =  predicted_chance_d20_crit * (101 - d100_fatal_crit_cutoff ) / 100
+
 ##########################################
-# Start run loop
+# Loop to execute number_of_runs runs of the simulation
 run_loop_counter = 0
 for run_loop_counter in range(0,number_of_runs):
+    if output_each_roll:
+        print('\n\n----------------------------------------------------------------------------------- Starting run ->', run_loop_counter + 1)
 
     #initialise fumble and crit counters
     d20_fumble_counter = 0
@@ -64,95 +87,162 @@ for run_loop_counter in range(0,number_of_runs):
     d100_crit_fatal_outcome = 0
 
     # define variables to track and pyplot
-    plot_turn_number_X = []
+    plot_round_number_X = []
     plot_total_number_of_fatal_outcomes_Y = []
 
-    # Get OS-generated random number as seed
-    random_data = os.urandom(64)
-    seed = int.from_bytes(random_data, byteorder="big")
-    random.seed(seed)
-
     ###################################################################
-    # Now, start the main loop to run many iterations of the simulation
-    loop_counter = 0
-    for loop_counter in range(0,number_of_turns):
+    # Loop to execute number_of_rounds rounds
+    round_loop_counter = 0
+    for round_loop_counter in range(0,number_of_rounds):
+        if output_each_roll:
+            print('round ', round_loop_counter)
+
+        # Get OS-generated random number as seed before each round
+        random_data = os.urandom(64)
+        seed = int.from_bytes(random_data, byteorder="big")
+        random.seed(seed)
 
         # Player roll section
         # Simulates a player attacking a monster
 
-        # roll the d20 and check the result
-        D20result = rollD20()
-    #    print(loop_counter, ' ', D20result)
+        # Loop to execute number_of_players player rolls
+        player_loop_counter = 0
+        for player_loop_counter in range(0,number_of_players):
+            # roll the d20 and check the result
+            D20result = rollD20()
+            if output_each_roll:
+                print('   player ', player_loop_counter + 1, ' -> ', D20result)
 
-        # If fumble, enter d20 fumble loop
-        if D20result <= d20_fumble_cutoff:
-            d20_fumble_counter += 1 # increment the d20_fumble_cutoff counter
-            D100result = rollD100()
-    #        print('   d20 Fumble!')
-    #        print('   D100 roll--> ', D100result)
+            # If fumble, enter d20 fumble loop
+            if D20result <= d20_fumble_cutoff:
+                d20_fumble_counter += 1 # increment the d20_fumble_cutoff counter
+                D100result = rollD100()
+                if output_each_roll:
+                    print('-------------------d20 Fumble!')
+                    print('-------------------D100 roll--> ', D100result)
 
-            # ... and enter the D100 check loop
-            if D100result >= d100_fatal_fumble_cutoff:
-                d100_fumble_fatal_outcome +=1 # increment the d100_fumble_fatal_outcome counter
-    #            print('   FATAL d100 FUMBLE!-------------------------------')
+                # ... and enter the D100 check loop
+                if D100result >= d100_fatal_fumble_cutoff:
+                    d100_fumble_fatal_outcome +=1 # increment the d100_fumble_fatal_outcome counter
+                    if output_each_roll:
+                        print('-----------------------------------------FATAL d100 FUMBLE!------------------')
+                        print('-----------------------------------------------------------------------------')
+
 
         # DM roll section
         # Simulates the DM rolling for a monster to attack a player
 
         # roll the d20 and check the result
         D20result = rollD20()
+        if output_each_roll:
+            print('   DM attack  -> ', D20result)
         # If crit, enter d20 crit loop
         if D20result >= d20_crit_cutoff:
             d20_crit_counter += 1 # increment the d20_crit_cutoff counter
             D100result = rollD100()
-    #        print('   d20 Crit!')
-    #        print('   D100 roll--> ', D100result)
+            if output_each_roll:
+                print('-------------------d20 Crit!')
+                print('-------------------D100 roll--> ', D100result)
 
             # ... and enter the D100 check loop
             if D100result >= d100_fatal_crit_cutoff:
                 d100_crit_fatal_outcome +=1 # increment the d100_crit_fatal_outcome counter
-    #            print('   FATAL d100 CRIT!-------------------------------')
+                if output_each_roll:
+                    print('---------------------------------------------FATAL d100 CRIT!-------------------')
+                    print('--------------------------------------------------------------------------------')
 
-        # Append values for plotting
-        plot_turn_number_X.append(loop_counter)
+        # Append x and y values for plotting this round's total fatal outcomes
+        plot_round_number_X.append(round_loop_counter)
         plot_total_number_of_fatal_outcomes_Y.append(d100_fumble_fatal_outcome + d100_crit_fatal_outcome)
 
-    ################################################################################
-    #Calculate mathematical odds for later comparison with stats from generated data
-    predicted_chance_fatal_fumble = (d20_fumble_cutoff / 20) * ( (101 - d100_fatal_fumble_cutoff ) / 100 )
-    predicted_chance_fatal_crit =  ( (21 - d20_crit_cutoff ) / 20 ) * ( (101 - d100_fatal_crit_cutoff ) / 100 )
+    ######################
+    # End the rounds loop
 
-    #########################################
-    # Calculate and print stats for whole run
-    chance_fatal_fumble = d100_fumble_fatal_outcome / number_of_turns
-    chance_fatal_crit = d100_crit_fatal_outcome / number_of_turns
-    print('\n-------------------------------------')
-    print('Run number', run_loop_counter)
-    print('  Results: In', number_of_turns, 'turns')
+    #####################################################
+    # Calculate and print (per round) stats for whole run
+    # from observed simulated data
+    chance_d20_fumble = d20_fumble_counter / number_of_rounds
+    chance_d20_crit = d20_crit_counter / number_of_rounds
+    chance_fatal_fumble = d100_fumble_fatal_outcome / number_of_rounds
+    chance_fatal_crit = d100_crit_fatal_outcome / number_of_rounds
+
+    print('\n\n\n-----------------------------------------------------------------------------------------------------')
+    print('--------------------------- Summary stats for run number', run_loop_counter + 1)
+    print('---------------------------', number_of_rounds, 'rounds with', number_of_players, 'players\n')
+    print('--------------------------- ')
+
+    # d20 fumble and crit stats per round, observed vs predicted
+    print('d20 fumble and crit stats per round, observed vs predicted')
+    print('----------------------------------------------------------\n')
+
+    # d20 fumble stats per round, observed vs predicted
+    if chance_d20_fumble > 0:
+        print('  -- chance of d20 fumble per round from generated data is roughly 1 /', round (1 / chance_d20_fumble, 0), ' vs. 1 /', round (1 / predicted_chance_d20_fumble, 0), ' predicted (abs number of d20 fumbles is', d20_fumble_counter, ')\n')
+    else:
+        print('  -- chance of d20 fumble per round from generated data is 0 vs. 1/', round (1 / predicted_chance_d20_fumble, 0), ' predicted (abs number of d20 fumbles is 0)\n')
+
+    # d20 crit stats per round, observed vs predicted
+    if chance_d20_crit > 0:
+        print('  -- chance of d20 crit per round from generated data is roughly 1 / ', round (1 / chance_d20_crit, 0), ' vs. 1 /', round (1 / predicted_chance_d20_crit, 0), ' predicted (abs number of d20 crits is', d20_crit_counter, ')\n\n')
+    else:
+        print('  -- chance of d20 crit per round from generated data is 0  vs. 1 /', round (1 / predicted_chance_d20_crit, 0) , ' predicted (abs number of d20 crits is 0)\n\n')
+
+    # fatal fumble and crit stats per round, observed vs predicted
+    print('fatal fumble and crit stats per round, observed vs predicted')
+    print('------------------------------------------------------------\n')
+
+    # fatal fumble stats per round, observed vs predicted
+
     if chance_fatal_fumble > 0:
-        print('  -- chance of fatal fumble per turn from generated data is roughly 1 / ', round (1 / chance_fatal_fumble, 0), '(abs number of fatal fumbles is', d100_fumble_fatal_outcome, ')')
+        print('  -- chance of fatal fumble per round from generated data is roughly 1 / ', round (1 / chance_fatal_fumble, 0), ' vs. 1 /', round (1 / predicted_chance_fatal_fumble, 0) , ' predicted (abs number of fatal fumbles is', d100_fumble_fatal_outcome, ')\n')
     else:
-        print('  -- chance of fatal fumble per turn from generated data is 0, (abs number of fatal fumbles is 0)')
-    print('  -----> prediced mathematical odds for this are roughly 1 / ', round (1 / predicted_chance_fatal_fumble, 0) )
+        if predicted_chance_fatal_fumble > 0:
+            print('  -- chance of fatal fumble per round from generated data is 0 vs. 1 /', round (1 / predicted_chance_fatal_fumble, 0) , ' predicted (abs number of fatal fumbles is 0)\n')
+        else:
+            print('  -- chance of fatal fumble per round from generated data is 0 vs. 0 predicted (abs number of fatal fumbles is 0)\n')
+
+    # fatal crit stats per round, observed vs predicted
     if chance_fatal_crit > 0:
-        print('  -- chance of fatal crit per turn generated data is roughly 1 / ', round (1 / chance_fatal_crit, 0), '(abs number of fatal crits is', d100_crit_fatal_outcome, ')')
+        print('  -- chance of fatal crit per round from generated data is roughly 1 / ', round (1 / chance_fatal_crit, 0), ' vs. 1 /', round (1 / predicted_chance_fatal_crit, 0) , ' predicted (abs number of fatal crits is', d100_crit_fatal_outcome, ')\n\n')
     else:
-        print('  -- chance of fatal crit per turn from generated data is 0, (abs number of fatal crits is 0)')
-    print('  -----> predicted mathematical odds for this are roughly 1 / ', round (1 / predicted_chance_fatal_crit, 0) )
-    print('  -- TOTAL chance of fatal crit per turn generated data is roughly 1 /', round (1 / (chance_fatal_fumble + chance_fatal_crit), 0), '(abs number of fatal crits is', d100_fumble_fatal_outcome + d100_crit_fatal_outcome, ')')
-    print('  -----> predicted mathematical odds for this are roughly 1 / ', round (1 / (predicted_chance_fatal_fumble + predicted_chance_fatal_crit), 0) )
+        if predicted_chance_fatal_crit > 0:
+            print('  -- chance of fatal crit per round from generated data is 0 vs. 1 /', round (1 / predicted_chance_fatal_crit, 0) , ' predicted  (abs number of fatal crits is 0)\n')
+        else:
+            print('  -- chance of fatal crit per round from generated data is 0 vs. 0 predicted  (abs number of fatal crits is 0)\n')
+
+    print('total fatal fumble and crit stats per round, observed vs predicted')
+    print('------------------------------------------------------------------\n')
+
+    # total fatal outcome stats per round, observed vs. predicted
+    if chance_fatal_fumble + chance_fatal_crit > 0:
+        print('  -- TOTAL chance of fatal outcome per round from generated data is roughly 1 /', round (1 / (chance_fatal_fumble + chance_fatal_crit), 0), ' vs. 1 /', round (1 / (predicted_chance_fatal_fumble + predicted_chance_fatal_crit), 0) , ' predicted (abs number of fatal crits is', d100_fumble_fatal_outcome + d100_crit_fatal_outcome, ')\n')
+    else:
+        print('  -- TOTAL chance of fatal outcome per round from generated data is 0 vs. 0 predicted (abs number of fatal crits is 0)\n')
+
 
     ########################################################
-    # Plot total number of fatal outcomes versus turn number
-    plt.plot(plot_turn_number_X,plot_total_number_of_fatal_outcomes_Y)
+    # Plot total number of fatal outcomes versus round number
+    plt.plot(plot_round_number_X,plot_total_number_of_fatal_outcomes_Y)
+
+##############
 # End run loop
 
+# Finalise and draw plot with axis labels and title
 plt.ylabel('Running total of fatal outcomes')
-plt.xlabel('No of turns taken')
-plot_title = ('Monte Carlo Simulation of fatal crits and fumbles over time\n' + str(number_of_runs) + ' run(s) of ' +
-        str(number_of_turns) + ' turns | fumble cutoff ' + str(d100_fatal_fumble_cutoff) + ' (odds/turn 1/' +
-        str(round (1 / predicted_chance_fatal_fumble, 0)) + ')\n| crit cutoff ' + str(d100_fatal_crit_cutoff) +
-        ' (odds/turn 1/' + str(round (1 / predicted_chance_fatal_crit, 0)) + ') | total odds/turn 1/' +
+plt.xlabel('No of rounds taken')
+# If predicted probabilities > 0 then express as 1/n
+if ( (predicted_chance_fatal_fumble > 0) and (predicted_chance_fatal_crit > 0) ):
+    plot_title = ('Monte Carlo Simulation of fatal crits and fumbles over time\nVerticals represent fatal outcomes for characters from fumbles they make or crits against them\n' + str(number_of_runs) + ' run(s) of ' +
+        str(number_of_rounds) + ' rounds & ' + str(number_of_players) + ' player(s) | fatal fumble d100 >= ' + str(d100_fatal_fumble_cutoff) + ' (ideal odds/round 1/' +
+        str(round (1 / predicted_chance_fatal_fumble, 0)) + ')\n| fatal crit d100 >= ' + str(d100_fatal_crit_cutoff) +
+        ' (ideal odds/round 1/' + str(round (1 / predicted_chance_fatal_crit, 0)) + ') | total ideal fatal odds/round 1/' +
         str(round (1 / (predicted_chance_fatal_fumble + predicted_chance_fatal_crit))))
+# ... otherwise use decimal expression to avoid div by zero
+else:
+        plot_title = ('Monte Carlo Simulation of fatal crits and fumbles over time\nVerticals represent fatal outcomes for characters from fumbles they make or crits against them\n' + str(number_of_runs) + ' run(s) of ' +
+            str(number_of_rounds) + ' rounds & ' + str(number_of_players) + ' player(s) | fatal fumble d100 >= ' + str(d100_fatal_fumble_cutoff) + ' (ideal odds/round ' +
+            str(round (predicted_chance_fatal_fumble, 4)) + ')\n| fatal crit d100 >= ' + str(d100_fatal_crit_cutoff) +
+            ' (ideal odds/round ' + str(round (predicted_chance_fatal_crit, 4)) + ') | total ideal fatal odds/round ' +
+            str(round ((predicted_chance_fatal_fumble + predicted_chance_fatal_crit), 4)))
 plt.title(plot_title)
 plt.show()
